81edf71c75b7c3438f38d5a92bbcbfab8d49936c
Maksim <max.detchik@gmail.com>
2021-09-04 15:30:28 +0300

    add realization of adapter item click

--- app/src/main/java/com/example/homework5_contacts/ui/contactList/ContactListFragment.kt
+++ app/src/main/java/com/example/homework5_contacts/ui/contactList/ContactListFragment.kt
@@ -1,12 +1,16 @@
 package com.example.homework5_contacts.ui.contactList
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.homework5_contacts.R
 import com.example.homework5_contacts.adapter.ContactListAdapter
+import com.example.homework5_contacts.entity.Contact
 import com.example.homework5_contacts.entity.Contacts
 
 class ContactListFragment:Fragment(R.layout.fragment_contact_list) {
@@ -16,10 +20,16 @@ class ContactListFragment:Fragment(R.layout.fragment_contact_list) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         rvContacts = view.findViewById(R.id.rv_contact_list)
-        contactListAdapter = ContactListAdapter(Contacts.list)
+        contactListAdapter = ContactListAdapter(Contacts.list) { contact -> onContactClick(contact) }
         rvContacts.apply {
             layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
             adapter = contactListAdapter
         }
+    }
+
+    private fun onContactClick(contact: Contact){
+        val bundle = bundleOf("contact" to contact)
+        view?.findNavController()?.navigate(R.id.action_contactListFragment_to_contactDetailsFragment,bundle)
+        Log.d("Fragment1", (bundle.get("contact") as Contact).name)
     }
 }
