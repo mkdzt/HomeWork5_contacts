b3e5b50529677d40ea20918beb7400f93bdacd24
Maksim <max.detchik@gmail.com>
2021-09-04 11:01:12 +0300

    add contact list adapter

---
+++ app/src/main/java/com/example/homework5_contacts/ContactListAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.homework5_contacts
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactListAdapter(private val contacts:List<Contact>) : RecyclerView.Adapter<ContactListAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvName: TextView = view.findViewById(R.id.tv_name)
+        val tvSurname: TextView = view.findViewById(R.id.tv_surname)
+        val tvPhone: TextView = view.findViewById(R.id.tv_phone)
+    }
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val view = LayoutInflater.from( parent.context)
+            .inflate(R.layout.contact_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvName.text = contacts[position].name
+        holder.tvSurname.text = contacts[position].surname
+        holder.tvPhone.text = contacts[position].phone
+    }
+
+    override fun getItemCount() = contacts.size
+}
