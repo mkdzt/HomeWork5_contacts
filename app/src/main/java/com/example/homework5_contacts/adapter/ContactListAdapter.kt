5ddd29e8156e5297b74c74e7bfb1c39fb3646fed
Maksim <max.detchik@gmail.com>
2021-09-04 15:22:31 +0300

    change contact adapter logic

--- app/src/main/java/com/example/homework5_contacts/adapter/ContactListAdapter.kt
+++ app/src/main/java/com/example/homework5_contacts/adapter/ContactListAdapter.kt
@@ -4,33 +4,62 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.homework5_contacts.R
 import com.example.homework5_contacts.entity.Contact
 
-class ContactListAdapter(private val contacts:List<Contact>) : RecyclerView.Adapter<ContactListAdapter.ViewHolder>() {
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val tvName: TextView = view.findViewById(R.id.tv_name)
-        val tvSurname: TextView = view.findViewById(R.id.tv_surname)
-        val tvPhone: TextView = view.findViewById(R.id.tv_phone)
-    }
+class ContactListAdapter(
+    private val contacts:List<Contact> = emptyList(),
+    private val onItemClicked:(Contact) -> Unit) : ListAdapter<Contact, ViewHolder>(ContactsDiffCallback) {
 
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
-        viewType: Int
+        viewType: Int,
     ): ViewHolder {
         val view = LayoutInflater.from( parent.context)
             .inflate(R.layout.contact_item, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view, onItemClicked)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.tvName.text = contacts[position].name
-        holder.tvSurname.text = contacts[position].surname
-        holder.tvPhone.text = contacts[position].phone
+        val contact = contacts[position]
+        holder.bind(contact)
     }
 
     override fun getItemCount() = contacts.size
+}
+
+class ViewHolder(view: View, private val onItemClicked: (Contact) -> Unit) : RecyclerView.ViewHolder(view){
+    private val tvName: TextView = view.findViewById(R.id.tv_name)
+    private val tvSurname: TextView = view.findViewById(R.id.tv_surname)
+    private val tvPhone: TextView = view.findViewById(R.id.tv_phone)
+    private var currentContact:Contact? = null
+
+    init {
+        itemView.setOnClickListener{
+            currentContact?.let{
+                onItemClicked(it)
+            }
+        }
+    }
+
+    fun bind(contact:Contact){
+        currentContact = contact
+        tvName.text = contact.name
+        tvSurname.text = contact.surname
+        tvPhone.text = contact.phone
+    }
+}
+
+object ContactsDiffCallback : DiffUtil.ItemCallback<Contact>() {
+    override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+        return oldItem.id == newItem.id
+    }
 }
