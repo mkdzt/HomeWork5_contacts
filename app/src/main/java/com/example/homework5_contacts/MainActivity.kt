62c19664f23adeed7d9a993981e603c0adc2d811
Maksim <max.detchik@gmail.com>
2021-09-04 08:55:14 +0300

    set app theme

--- app/src/main/java/com/example/homework5_contacts/MainActivity.kt
+++ app/src/main/java/com/example/homework5_contacts/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_HomeWork5_contacts)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
