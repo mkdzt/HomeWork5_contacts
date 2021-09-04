80f74317349d513929e54da69e63e37440a03ac1
Maksim <max.detchik@gmail.com>
2021-09-04 10:59:49 +0300

    add entity contact

---
+++ app/src/main/java/com/example/homework5_contacts/Contact.kt
@@ -0,0 +1,3 @@
+package com.example.homework5_contacts
+
+data class Contact(val id:String,val name:String,val surname:String,val phone:String)
---
+++ app/src/main/java/com/example/homework5_contacts/Contacts.kt
@@ -0,0 +1,30 @@
+package com.example.homework5_contacts
+
+object Contacts {
+    val list = listOf(
+        Contact(
+            id = "0",
+            name = "Anna",
+            surname = "Ivanova",
+            phone = "80259632588"
+        ),
+        Contact(
+            id = "1",
+            name = "Sveta",
+            surname = "Rubirshtern",
+            phone = "80335874124"
+        ),
+        Contact(
+            id = "2",
+            name = "Alexander",
+            surname = "Trofimov",
+            phone = "80152456985"
+        ),
+        Contact(
+            id = "3",
+            name = "Kolyan",
+            surname = "Dolzhanskij",
+            phone = "80294578963"
+        )
+    )
+}
